39dfce668b1e94f8ace7a0446f436a16b0a47a43
ff-0xff <61967496+ff-0xff@users.noreply.github.com>
2021-12-08 00:26:26 +0800

    Update handler.py

--- tests/pressure_tests/pt-1/pt/handler.py
+++ tests/pressure_tests/pt-1/pt/handler.py
@@ -3,84 +3,76 @@ import random
 import time
 
 def handle(req):
-    repeat_counter = 10
+    repeat_counter = 3
+    data_size = 100
     
     a = api.Agent()
     ns_name = "test_ns"
-    test_count = 11*repeat_counter
-    count_ok = test_count
-    time_start = time.time()
-    task_ok = [repeat_counter] * 11
+    # test_count = 11 * repeat_counter
+    # count_ok = test_count
+    # task_ok = [repeat_counter] * 11
+    key = ["key"+str(random.randint(1,data_size)) for i in range(data_size)]
+    value = [("val"+str(random.randint(1,data_size))).encode() for i in range(data_size)]
+    key_exist = random.shuffle(["key" + str(i) for i in range(data_size)])
+    key_delete = random.shuffle(list(set(key)))
+    time_set = [0] * repeat_counter
+    time_exists = [0] * repeat_counter
+    time_get = [0] * repeat_counter
+    time_delete = [0] * repeat_counter
+    time_all_start = time.time()
+    
+    c, i = a.create_ns(ns_name)
+    if c != 0:
+        count_ok -= 1
+        task_ok[0] = task_ok[0] - 1
+
+    c, i = a.connect_ns(ns_name)
+    if c != 0:
+        count_ok -= 1
+        task_ok[1] = task_ok[1] - 1
 
     for temp_i in range(repeat_counter):
-        rand = str(random.randint(1,1000))
-        key = "test_key_" + rand
-        value = ("test_value" + rand).encode()
         
-        c, i = a.create_ns(ns_name)
-        if c != 0:
-            count_ok -= 1
-            task_ok[0] = task_ok[0] - 1
+        time_temp_start = time.time()
+        for temp_j in range(data_size):
+            c, i = a.set(key[temp_j], value[temp_j])
+        time_temp_end = time.time()
+        time_set[temp_i] = time_temp_end - time_temp_start
+
+        time_temp_start = time.time()
+        for ke in key_exist:
+            c, i = a.exists(ke)
+        time_temp_end = time.time()
+        time_exists[temp_i] = time_temp_end - time_temp_start
+
+        time_temp_start = time.time()
+        for ke in key_exist:
+            c, i = a.get(ke)
+        time_temp_end = time.time()
+        time_get[temp_i] = time_temp_end - time_temp_start
         
-        c, i = a.connect_ns(ns_name)
-        if c != 0:
-            count_ok -= 1
-            task_ok[1] = task_ok[1] - 1
+        time_temp_start = time.time()
+        for kd in key_delete:
+            c, i = a.delete(kd)
+        time_temp_end = time.time()
+        time_delete[temp_i] = time_temp_end - time_temp_start
 
-        c, i = a.set(key, value)
-        if c != 0:
-            count_ok -= 1
-            task_ok[2] = task_ok[2] - 1
+    c, i = a.delete_ns(ns_name)
+    if c == 0:
+        count_ok -= 1
+        task_ok[10] = task_ok[10] - 1
 
-        c, i = a.exists(key)
-        if c != 0:
-            count_ok -= 1
-            task_ok[3] = task_ok[3] - 1
-
-        c, i, v = a.get(key)
-        if c != 0:
-            count_ok -= 1
-            task_ok[4] = task_ok[4] - 1
-        elif v != value:
-            count_ok -= 1
-            task_ok[4] = task_ok[4] - 1
-
-        c, i = a.delete(key)
-        if c != 0:
-            count_ok -= 1
-            task_ok[5] = task_ok[5] - 1
-            
-        c, i = a.exists(key)
-        if c == 0:
-            count_ok -= 1
-            task_ok[6] = task_ok[6] - 1
-
-        c, i, v = a.get(key)
-        if c == 0:
-            count_ok -= 1
-            task_ok[7] = task_ok[7] - 1
-
-        c, i = a.delete_ns(ns_name)
-        if c != 0:
-            count_ok -= 1
-            task_ok[8] = task_ok[8] - 1
-
-        c, i = a.connect_ns(ns_name)
-        if c == 0:
-            count_ok -= 1
-            task_ok[9] = task_ok[9] - 1
-        
-        c, i = a.delete_ns(ns_name)
-        if c == 0:
-            count_ok -= 1
-            task_ok[10] = task_ok[10] - 1
-
-    if count_ok == test_count :
-        res = "ok"
-    else :
-        res = "failed"
-        print(task_ok)
-    time_end = time.time()
-    print("pressure test time: {} seconds; ".format(time_end - time_start))
-    print("test result: {0}. {1} passed; {2} failed".format(res, count_ok, test_count - count_ok))
+    time_all_end = time.time()
+    # if count_ok == test_count :
+        # res = "ok"
+    # else :
+        # res = "failed"
+        # print(task_ok)
+    print("pressure test time: {} seconds; ".format(time_all_end - time_all_start))
+    print("set: {}".format(time_set))
+    print("exists: {}".format(time_exists))
+    print("get: {}".format(time_get))
+    print("deete: {}".format(time_delete))
+    
+    # print("test result: {0}. {1} passed; {2} failed".format(res, count_ok, test_count - count_ok))
     return req
