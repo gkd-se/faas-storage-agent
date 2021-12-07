33c32089671b759708608682b1bd8427e941e194
ff-0xff <61967496+ff-0xff@users.noreply.github.com>
2021-12-07 20:46:00 +0800

    Update handler.py

--- tests/pressure_tests/pt-1/pt/handler.py
+++ tests/pressure_tests/pt-1/pt/handler.py
@@ -3,12 +3,14 @@ import random
 import time
 
 def handle(req):
+    repeat_counter = 10
+    
     a = api.Agent()
     ns_name = "test_ns"
-    repeat_counter = 2
     test_count = 11*repeat_counter
     count_ok = test_count
     time_start = time.time()
+    task_ok = [repeat_counter] * 11
 
     for temp_i in range(repeat_counter):
         rand = str(random.randint(1,1000))
@@ -18,86 +20,66 @@ def handle(req):
         c, i = a.create_ns(ns_name)
         if c != 0:
             count_ok -= 1
-            print("create_ns test ... failed. err_info:", i)
-        else:
-            print("create_ns test ... ok")
+            task_ok[0] = task_ok[0] - 1
         
         c, i = a.connect_ns(ns_name)
         if c != 0:
             count_ok -= 1
-            print("connect_ns test ... failed. err_info:", i)
-        else:
-            print("connect_ns test ... ok")
+            task_ok[1] = task_ok[1] - 1
 
         c, i = a.set(key, value)
         if c != 0:
             count_ok -= 1
-            print("set test ... failed. err_info:", i)
-        else:
-            print("set test ... ok")
+            task_ok[2] = task_ok[2] - 1
 
         c, i = a.exists(key)
         if c != 0:
             count_ok -= 1
-            print("exists test ... failed. err_info:", i)
-        else:
-            print("exists test ... ok")
+            task_ok[3] = task_ok[3] - 1
 
         c, i, v = a.get(key)
         if c != 0:
-            print("get test ... failed. err_info:", i)
+            count_ok -= 1
+            task_ok[4] = task_ok[4] - 1
         elif v != value:
             count_ok -= 1
-            print("get test ... failed. err value : ", v.decode())
-        else:
-            print("get test ... ok")
+            task_ok[4] = task_ok[4] - 1
 
         c, i = a.delete(key)
         if c != 0:
             count_ok -= 1
-            print("delete test ... failed. err_info:", i)
-        else:
-            print("delete test ... ok")
+            task_ok[5] = task_ok[5] - 1
             
         c, i = a.exists(key)
         if c == 0:
             count_ok -= 1
-            print("exists test ... failed. err_info:", i)
-        else:
-            print("exists test ... ok")
+            task_ok[6] = task_ok[6] - 1
 
         c, i, v = a.get(key)
         if c == 0:
             count_ok -= 1
-            print("get test ... failed. err_info:", i)
-        else:
-            print("get test ... ok")
+            task_ok[7] = task_ok[7] - 1
 
         c, i = a.delete_ns(ns_name)
         if c != 0:
             count_ok -= 1
-            print("delete_ns test ... failed. err_info:", i)
-        else:
-            print("delete_ns test ... ok")
+            task_ok[8] = task_ok[8] - 1
 
         c, i = a.connect_ns(ns_name)
         if c == 0:
             count_ok -= 1
-            print("connect_ns test ... failed. err_info:", i)
-        else:
-            print("connect_ns test ... ok")
+            task_ok[9] = task_ok[9] - 1
         
         c, i = a.delete_ns(ns_name)
         if c == 0:
             count_ok -= 1
-            print("delete_ns test ... failed. err_info:", i)
-        else:
-            print("delete_ns test ... ok")
+            task_ok[10] = task_ok[10] - 1
 
     if count_ok == test_count :
         res = "ok"
     else :
         res = "failed"
+        print(task_ok)
     time_end = time.time()
     print("pressure test time: {} seconds; ".format(time_end - time_start))
     print("test result: {0}. {1} passed; {2} failed".format(res, count_ok, test_count - count_ok))
